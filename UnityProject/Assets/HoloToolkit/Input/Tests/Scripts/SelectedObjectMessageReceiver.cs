3ab37b4ba9029b3a8b8950a8507e7de90e1b0810
Mike Taulty <mtaulty@microsoft.com>
2017-03-24 21:58:54 +0000

    Adding Unity Project

---
+++ UnityProject/Assets/HoloToolkit/Input/Tests/Scripts/SelectedObjectMessageReceiver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace HoloToolkit.Unity.InputModule.Tests
+{
+    /// <summary>
+    /// SelectedObjectMessageReceiver class shows how to handle messages sent by SelectedObjectMessageSender.
+    /// This particular implementatoin controls object appearance by changing its color when selected.
+    /// </summary>
+    public class SelectedObjectMessageReceiver : MonoBehaviour
+    {
+        [Tooltip("Object color changes to this when selected.")] public Color SelectedColor = Color.red;
+
+        private Material material;
+        private Color originalColor;
+
+        private void Start()
+        {
+            material = GetComponent<Renderer>().material;
+            originalColor = material.color;
+        }
+
+        public void OnSelectObject()
+        {
+            material.color = SelectedColor;
+        }
+
+        public void OnClearSelection()
+        {
+            material.color = originalColor;
+        }
+    }
+}
---
+++ UnityProject/Assets/Placeholder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeholder : MonoBehaviour
+{
+  public Transform textMeshObject;
+
+  private void Start()
+  {
+    this.textMesh = this.textMeshObject.GetComponent<TextMesh>();
+    this.OnReset();
+  }
+  public void OnScan()
+  {
+    this.textMesh.text = "scanning for 30s";
+
+#if !UNITY_EDITOR
+    MediaFrameQrProcessing.Wrappers.ZXingQrCodeScanner.ScanFirstCameraForQrCode(
+        result =>
+        {
+          UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+          {
+            this.textMesh.text = result?.Text ?? "not found";
+          }, 
+          false);
+        },
+        TimeSpan.FromSeconds(30));
+#endif
+  }
+  public void OnReset()
+  {
+    this.textMesh.text = "say scan to start";
+  }
+  TextMesh textMesh;
+}
